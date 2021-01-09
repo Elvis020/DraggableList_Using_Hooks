f79a2924a391cbf92eb2a398e978ffa9dbc9f16c
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-01-09 10:49:13 +0000

    🎃 Added dragOver Function 🎃

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const initList = [1, 2, 3, 4, 5, 6];
 
 function App() {
+  const [list, setList] = useState(initList);
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  const handleDrag = (e, index) => {
+    // When we start dragging we need to remember the item we are dragging.
+    // So we create a state for it.
+    setDraggedItem(list[index]);
+
+    // To make our drags look smoother
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  };
+
+  const handleDragOver = (index) => {
+    const draggedOverItem = list[index];
+    // Checking whether the draggedItem is the same as the item beign dragged.
+    if (draggedItem === draggedOverItem) return;
+
+    // Filtering the list to get the dragged item
+    const items = list.filter((i) => i !== draggedItem);
+
+    // Inserting the dragged item into the index we are dragging over.
+    items.splice(index, 0, draggedItem);
+    setList(items)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Drag and Drop Project using React Hooks</h3>
+        {list.map((listo, index) => (
+          <ul key={index}>
+            <li
+              onDragOver={() => handleDragOver(index)}
+              className="item-style"
+            >
+              {/* The item is intentionally put in a div to make it draggable */}
+              <div draggable onDragStart={(e) => handleDrag(e, index)}>
+                {listo}
+              </div>
+            </li>
+          </ul>
+        ))}
       </header>
     </div>
   );
